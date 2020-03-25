be7718aba0173a94793757e57966402254fd1f95
Rob Mathews <robbis@gmail.com>
2020-03-25 17:31:28 +0000

    finished extension

---
+++ models/traveller.js
@@ -0,0 +1,37 @@
+const Traveller = function(journeys) {
+	this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+	return this.journeys.map(journey => journey.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function() {
+	return this.journeys.map(journey => journey.endLocation);
+};
+
+Traveller.prototype.getJourneysByTransport = function(transport) {
+	return this.journeys.filter(journey => journey.transport === transport);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function(minDistance) {
+	return this.journeys.filter(journey => journey.distance >= minDistance);
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function() {
+	return this.journeys.reduce((total, journey) => total + journey.distance, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function() {
+	// const methodsOfTransport = this.journeys.map(journey => journey.transport);
+
+	// return methodsOfTransport.filter(
+	// 	(transport, index) => methodsOfTransport.indexOf(transport) === index
+	// );
+
+	return (uniqueList = [
+		...new Set(this.journeys.map(journey => journey.transport))
+	]);
+};
+
+module.exports = Traveller;
